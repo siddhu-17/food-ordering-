c7d84b174c7e34448161104ab51e2046b89324c3
santhoshn20004 <127004239+santhoshn20004@users.noreply.github.com>
2023-07-08 19:25:31 +0530

    Update register.php

--- account/register.php
+++ account/register.php
@@ -1,11 +1,5 @@
 <?php  
-session_start(); 
-if(isset($_SESSION['admin_sid']) || isset($_SESSION['customer_sid']))
-{
-	header("location:index.php");
-}
-else{
-?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
